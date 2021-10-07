3c816fad390c0bfb68795d2de58b482c43c3a495
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-10-07 21:57:18 +0300

    Starting core skeleton of patching game

--- open_prime_rando/cli.py
+++ open_prime_rando/cli.py
@@ -1,8 +1,18 @@
 import argparse
+from pathlib import Path
+
+from open_prime_rando import echoes_patcher
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument("--game", required=True, choices=["echoes"])
+    parser.add_argument("--input-paks", required=True, type=Path,
+                        help="Path to where the paks to randomizer can be found.")
+    parser.add_argument("--output-paks", required=True, type=Path,
+                        help="Path to where the modified paks will be written to.")
+    parser.add_argument("--input-json", type=Path,
+                        help="Path to the configuration json. If missing, it's read from standard input")
     return parser
 
 
@@ -10,3 +20,9 @@ def main():
     parser = create_parser()
     args = parser.parse_args()
     print(args)
+
+    echoes_patcher.patch_paks(
+        args.input_paks,
+        args.output_paks,
+        {},
+    )
---
+++ open_prime_rando/echoes_patcher.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+
+def patch_paks(paks_path: Path, output_path: Path, configuration: dict):
+    pass
