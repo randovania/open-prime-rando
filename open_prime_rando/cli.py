84ab0ba67940bc15f747d115027aad9dc42e4721
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-10-08 01:00:00 +0300

    Have logging

--- open_prime_rando/cli.py
+++ open_prime_rando/cli.py
@@ -1,4 +1,6 @@
 import argparse
+import logging
+import logging.config
 from pathlib import Path
 
 from open_prime_rando import echoes_patcher
@@ -16,7 +18,39 @@ def create_parser():
     return parser
 
 
+def setup_logging():
+    handlers = {
+        'default': {
+            'level': 'DEBUG',
+            'formatter': 'default',
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://sys.stdout',  # Default is stderr
+        },
+    }
+    logging.config.dictConfig({
+        'version': 1,
+        'formatters': {
+            'default': {
+                'format': '[%(asctime)s] [%(levelname)s] [%(name)s] %(funcName)s: %(message)s',
+            }
+        },
+        'handlers': handlers,
+        'disable_existing_loggers': False,
+        'loggers': {
+            'default': {
+                'level': 'DEBUG',
+            },
+        },
+        'root': {
+            'level': 'DEBUG',
+            'handlers': list(handlers.keys()),
+        },
+    })
+    logging.info("Hello world.")
+
+
 def main():
+    setup_logging()
     parser = create_parser()
     args = parser.parse_args()
     print(args)
