b7140566410f431fbddf1c1ed6acb247b1c4a393
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-05-12 21:58:30 +0300

    Add PatcherEditor

---
+++ open_prime_rando/patcher_editor.py
@@ -0,0 +1,33 @@
+import typing
+
+from retro_data_structures.base_resource import BaseResource, NameOrAssetId
+from retro_data_structures.file_tree_editor import FileTreeEditor, FileProvider
+from retro_data_structures.formats import Mlvl
+from retro_data_structures.formats.mrea import Mrea
+from retro_data_structures.game_check import Game
+
+T = typing.TypeVar("T")
+
+
+class PatcherEditor(FileTreeEditor):
+    memory_files: dict[NameOrAssetId, BaseResource]
+
+    def __init__(self, provider: FileProvider):
+        super().__init__(provider, Game.ECHOES)
+        self.memory_files = {}
+
+    def get_file(self, path: NameOrAssetId, type_hint: typing.Type[T] = BaseResource) -> T:
+        if path not in self.memory_files:
+            self.memory_files[path] = self.get_parsed_asset(path, type_hint=type_hint)
+        return self.memory_files[path]
+
+    def get_mlvl(self, name: NameOrAssetId) -> Mlvl:
+        return self.get_file(name, Mlvl)
+
+    def get_mrea(self, name: NameOrAssetId) -> Mrea:
+        return self.get_file(name, Mrea)
+
+    def flush_modified_assets(self):
+        for name, resource in self.memory_files.items():
+            self.replace_asset(name, resource)
+        self.memory_files = {}
