bbe1f76bdd80c5a7e74f98ee5dafc44f7cda5506
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-06-17 15:46:12 +0300

    Add schema

--- open_prime_rando/echoes_patcher.py
+++ open_prime_rando/echoes_patcher.py
@@ -1,19 +1,25 @@
+import json
 import logging
 from pathlib import Path
 
 from retro_data_structures.asset_manager import FileProvider
 
-from open_prime_rando.echoes import custom_assets
-from open_prime_rando.echoes.dock_lock_rando import apply_door_rando
 from open_prime_rando.echoes.small_randomizations import apply_small_randomizations
-from open_prime_rando.echoes.specific_area_patches import specific_patches
 from open_prime_rando.patcher_editor import PatcherEditor
+from open_prime_rando.validator_with_default import DefaultValidatingDraft7Validator
 
 LOG = logging.getLogger("echoes_patcher")
 
 
+def _read_schema():
+    with Path(__file__).parent.joinpath("echoes", "schema.json").open() as f:
+        return json.load(f)
+
+
 def patch_paks(file_provider: FileProvider, output_path: Path, configuration: dict):
     LOG.info("Will patch files at %s", file_provider)
+
+    DefaultValidatingDraft7Validator(_read_schema()).validate(configuration)
 
     editor = PatcherEditor(file_provider)
 
