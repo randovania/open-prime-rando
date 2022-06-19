08ad3c05467ae183f51f09d7424ed7b7733d6209
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-06-19 14:55:11 +0300

    Add deps to MLVL deps

--- open_prime_rando/echoes/inverted/__init__.py
+++ open_prime_rando/echoes/inverted/__init__.py
@@ -1,3 +1,8 @@
+import dataclasses
+
+import construct
+from retro_data_structures.base_resource import Dependency
+from retro_data_structures.dependencies import recursive_dependencies_for_editor
 from retro_data_structures.enums import echoes
 from retro_data_structures.formats import Mlvl
 from retro_data_structures.properties.echoes.archetypes.EditorProperties import EditorProperties
@@ -12,26 +17,35 @@ from open_prime_rando.echoes.inverted.area_pairs import TG_PAIRS, AGON_PAIRS
 from open_prime_rando.patcher_editor import PatcherEditor
 
 
+def collect_dependencies(obj):
+    for field in dataclasses.fields(obj):
+        if "asset_types" in field.metadata:
+            yield getattr(obj, field.name)
+
+
+_all_deps_cache: dict[frozenset[int], set[Dependency]] = {}
+
+
 def _modify_world(world: Mlvl, pairs: list[tuple[int, int]]):
     for pair in pairs:
         light = world.get_area(pair[0])
         dark = world.get_area(pair[1])
 
-        module_dependencies = light._raw.module_dependencies
-        if "ScriptSafeZone.rel" not in module_dependencies.rel_module:
-            module_dependencies.rel_module.insert(0, "ScriptSafeZone.rel")
-            for i in range(1, len(module_dependencies.rel_offset)):
-                module_dependencies.rel_offset[i] += 1
-
         light_layer = light.get_layer("Default")
         dark_layer = dark.get_layer("Default")
 
-        special_ids = set()
+        assert light_layer.index == 0
 
+        new_dependencies = set()  # asset ids that must be added to the mlvl dependency list
+        special_ids = set()  # ids for objects that have extra functionality and we want to keep
         copied_safe_zones = {}
+
+        # Copy the safe zones
         for instance in list(dark_layer.instances):
             if instance.type == SafeZone:
-                copied_safe_zones[instance.id] = light_layer.add_instance_with(instance.get_properties())
+                copied_safe_zones[instance.id] = light_layer.add_instance_with(props := instance.get_properties())
+                assert isinstance(props, SafeZone)
+                new_dependencies.add(props.impact_effect)
 
                 ids = {conn.target for conn in instance.connections}
                 for target in ids:
@@ -42,9 +56,11 @@ def _modify_world(world: Mlvl, pairs: list[tuple[int, int]]):
                 if instance.id not in special_ids:
                     dark_layer.remove_instance(instance)
 
+        # Copy the safe zone crystals
         for instance in list(dark_layer.instances):
             if instance.type == SafeZoneCrystal:
-                copied_crystal = light_layer.add_instance_with(instance.get_properties())
+                copied_crystal = light_layer.add_instance_with(props := instance.get_properties())
+                new_dependencies.update(collect_dependencies(props))
 
                 targets_special = False
 
@@ -64,6 +80,8 @@ def _modify_world(world: Mlvl, pairs: list[tuple[int, int]]):
                 if not targets_special:
                     dark_layer.remove_instance(instance)
 
+        assert 0xffffffff not in new_dependencies
+
         # Area Attributes
         for a, new_value in zip([light, dark], [True, False]):
             for layer in a.layers:
@@ -76,22 +94,49 @@ def _modify_world(world: Mlvl, pairs: list[tuple[int, int]]):
 
         # Timer Stuff
         try:
-            dark.get_layer("Default").remove_instance("Timer_Start Dark")
-            dark.get_layer("Default").remove_instance("SpecialFunction_Darkworld")
+            dark_layer.remove_instance("Timer_Start Dark")
+            dark_layer.remove_instance("SpecialFunction_Darkworld")
         except KeyError:
             pass
 
-        light_timer = light.get_layer("Default").add_instance_with(Timer(
+        light_timer = light_layer.add_instance_with(Timer(
             editor_properties=EditorProperties(name="Timer_Start InvertedDark"),
             time=0.001,
             auto_start=True,
         ))
-        light_function = light.get_layer("Default").add_instance_with(SpecialFunction(
+        light_function = light_layer.add_instance_with(SpecialFunction(
             editor_properties=EditorProperties(name="SpecialFunction_Darkworld Inverted"),
             function=echoes.Function.Darkworld,
         ))
         light_timer.add_connection('ZERO', 'ACTN', light_function)
         light_timer.add_connection('ZERO', 'DECR', light_function)
+
+        # Update dependencies
+
+        # Add assets used by safe zones
+        new_dependencies = frozenset(new_dependencies)
+        if new_dependencies not in _all_deps_cache:
+            _all_deps_cache[new_dependencies] = recursive_dependencies_for_editor(
+                world.asset_manager, list(new_dependencies)
+            )
+
+        dependencies = light._raw.dependencies
+        assert isinstance(dependencies.dependencies_b, list)
+        for dep in _all_deps_cache[new_dependencies]:
+            dependencies.dependencies_b.append(construct.Container(
+                asset_id=dep.id,
+                asset_type=dep.type,
+            ))
+
+        for i in range(1, len(dependencies.dependencies_offset)):
+            dependencies.dependencies_offset[i] += len(new_dependencies)
+
+        # Add Safe Zone's module dep
+        module_dependencies = light._raw.module_dependencies
+        if "ScriptSafeZone.rel" not in module_dependencies.rel_module:
+            module_dependencies.rel_module.insert(0, "ScriptSafeZone.rel")
+            for i in range(1, len(module_dependencies.rel_offset)):
+                module_dependencies.rel_offset[i] += 1
 
 
 def apply_inverted(editor: PatcherEditor):
