cd9a3478b2cbb7c40ce0dfb036f47522a7173271
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-06-26 02:51:46 +0300

    Use our own names for areas instead of the name in MLVL

--- src/open_prime_rando/dynamic_schema.py
+++ src/open_prime_rando/dynamic_schema.py
@@ -2,7 +2,6 @@ import copy
 
 import open_prime_rando.echoes.asset_ids.world
 from open_prime_rando.patcher_editor import PatcherEditor
-from open_prime_rando.unique_area_name import get_name_for_area
 
 
 def expand_schema(base_schema: dict, editor: PatcherEditor) -> dict:
@@ -18,9 +17,13 @@ def expand_schema(base_schema: dict, editor: PatcherEditor) -> dict:
         world_def["properties"]["areas"] = {"type": "object", "additionalProperties": False, "properties": area_props}
 
         world_details = open_prime_rando.echoes.asset_ids.world.load_dedicated_file(world)
+        mrea_to_name: dict[int, str] = {
+            mrea: name
+            for name, mrea in world_details.NAME_TO_ID_MREA.items()
+        }
 
         for area in mlvl.areas:
-            area_name = get_name_for_area(area)
+            area_name = mrea_to_name[area.mrea_asset_id]
             area_def = copy.deepcopy(schema["$defs"]["area"])
             area_props[area_name] = area_def
 
