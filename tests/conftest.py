1899d76a16461e007c41e01a2f812498f497dcb6
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-06-25 17:49:35 +0300

    Add tests for dock lock rando

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ from retro_data_structures.game_check import Game
 
 from open_prime_rando.patcher_editor import PatcherEditor
 
-_FAIL_INSTEAD_OF_SKIP = False
+_FAIL_INSTEAD_OF_SKIP = True
 
 
 def get_env_or_skip(env_name):
@@ -25,13 +25,24 @@ def prime2_iso_provider():
 
 
 @pytest.fixture(scope="module")
-def prime2_editor(prime2_iso_provider):
+def raw_prime2_editor(prime2_iso_provider):
     return PatcherEditor(prime2_iso_provider, game=Game.ECHOES)
 
 
+@pytest.fixture()
+def prime2_editor(raw_prime2_editor):
+    editor = raw_prime2_editor
+    yield editor
+    editor.memory_files = {}
+    for custom_asset, asset_id in editor._custom_asset_ids.items():
+        editor._paks_for_asset_id.pop(asset_id)
+    editor._custom_asset_ids = {}
+    editor._modified_resources = {}
+
+
 def pytest_addoption(parser):
-    parser.addoption('--fail-if-missing', action='store_true', dest="fail_if_missing",
-                     default=False, help="Fails tests instead of skipping, in case any asset is missing")
+    parser.addoption('--skip-if-missing', action='store_false', dest="fail_if_missing",
+                     default=True, help="Skip tests instead of missing, in case any asset is missing")
 
 
 def pytest_configure(config: pytest.Config):
