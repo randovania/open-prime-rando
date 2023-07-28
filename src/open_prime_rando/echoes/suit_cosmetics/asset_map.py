21737f6322f923db7b0312f3108e75ef3c3a332e
duncathan <dunc@duncathan.com>
2023-07-27 19:01:07 -0600

    support custom suit textures

---
+++ src/open_prime_rando/echoes/suit_cosmetics/asset_map.py
@@ -0,0 +1,37 @@
+from retro_data_structures.base_resource import AssetId
+
+SUIT_ASSETS: dict[str, dict[AssetId, str]] = {
+    "varia": {
+        0x788432DA: "samus_varia_visor.TXTR",
+        0xC2DE6C2A: "samus_varia_visor_emissive.TXTR",
+        0x12B6539F: "samus_varia_banding.TXTR",
+        0x42DFBEBF: "samus_varia_banding_2.TXTR",
+        0x604D4DC5: "samus_varia_flex.TXTR",
+        0xE3001044: "samus_varia_flex_polish.TXTR",
+        0xE8AF7174: "samus_varia_armor.TXTR",
+        0xFFD9D212: "samus_varia_armor_polish.TXTR",
+        0x8FF8E517: "samus_varia_trim.TXTR",
+        0x2883811B: "samus_varia_trim_polish.TXTR",
+        0x355E4212: "samus_varia_metallic.TXTR",
+        0x0EF3BA76: "samus_varia_morph_shell.TXTR",
+        0x0CC73555: "samus_varia_morph_emissive.TXTR",
+        0x3C214DF7: "samus_varia_morph_banding.TXTR",
+        0xAAEC3745: "samus_varia_morph_metallic.TXTR",
+
+        0x1E5621F5: "samus_varia_low_res_armor.TXTR",
+        0x97FEE0A8: "samus_varia_low_res_armor_polish.TXTR",
+        0x23B19B14: "samus_varia_low_res_emissive.TXTR",
+        0xDD1473FC: "samus_varia_low_res_metallic.TXTR",
+        0x2777D332: "samus_varia_morph_2_shell.TXTR",
+        0xEB669581: "samus_varia_morph_2_shell_polish.TXTR",
+        0xA29AEF93: "samus_varia_morph_3_shell.TXTR",
+        0x9643B175: "samus_varia_morph_3_shell_polish.TXTR",
+        0x2F566CEC: "samus_morph_2_3_metallic.TXTR",
+    },
+    "dark": {
+        # TODO
+    },
+    "light": {
+        # TODO
+    },
+}
