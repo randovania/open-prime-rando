0de63ac0aab94f7c40cf6471eb5d17ca630b919a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-02-13 20:42:58 +0200

    PatcherEditor.get_pickup_scan

--- src/open_prime_rando/patcher_editor.py
+++ src/open_prime_rando/patcher_editor.py
@@ -1,27 +1,45 @@
 import typing
 
-from retro_data_structures.asset_manager import AssetManager
+from retro_data_structures.asset_manager import AssetManager, FileProvider
 from retro_data_structures.base_resource import AssetId, NameOrAssetId
 from retro_data_structures.formats.mlvl import Mlvl
 from retro_data_structures.formats.mrea import Area
+from retro_data_structures.formats.scan import Scan
 from retro_data_structures.formats.strg import Strg
 from retro_data_structures.game_check import Game
+from retro_data_structures.properties.echoes.objects.ScannableObjectInfo import ScannableObjectInfo
 
 T = typing.TypeVar("T")
 
 
 class PatcherEditor(AssetManager):
+    pooled_scans: dict[tuple[tuple[str, str, str], AssetId], AssetId]
+
+    def __init__(self, provider: FileProvider, game: Game):
+        super().__init__(provider, game)
+        self.pooled_scans = {}
+
     def get_mlvl(self, name: NameOrAssetId) -> Mlvl:
         return self.get_file(name, Mlvl)
 
     def get_area(self, mlvl: NameOrAssetId, mrea: NameOrAssetId) -> Area:
         return self.get_mlvl(mlvl).get_area(mrea)
 
+    def build_modified_files(self):
+        super().build_modified_files()
+        self.pooled_scans.clear()
+
     def create_strg(
         self,
         name: str,
         strings: str | typing.Iterable[str] = (),
     ) -> tuple[AssetId, Strg]:
+        if self.does_asset_exists(name):
+            existing = self.get_file(name, Strg)
+            if existing.strings == strings:
+                return [self._resolve_asset_id(name), existing]
+            raise ValueError(f"STRG named {name} already exists with different contents")
+
         template_id = None
         if self.target_game == Game.ECHOES:
             # Strings/Worlds/TempleHub/01_Temple_LandingSite.STRG
@@ -42,3 +60,29 @@ class PatcherEditor(AssetManager):
         strg.set_string_list(strings)
 
         return asset_id, strg
+
+    def get_pickup_scan(self, strings: str | tuple[str, str, str], model: AssetId) -> AssetId:
+        if isinstance(strings, str):
+            strings = (strings, "", "")
+
+        if (strings, model) not in self.pooled_scans:
+            template_id = None
+            if self.target_game == Game.ECHOES:
+                # Uncategorized/Light Beam.SCAN
+                template_id = 0x7427BA3C
+
+            if template_id is None:
+                raise NotImplementedError
+
+            name = f"Pickup{strings}{model}"
+
+            asset_id = self.duplicate_asset(template_id, f"{name}.SCAN")
+            scan = self.get_file(asset_id, Scan)
+
+            with scan.scannable_object_info.edit_properties(ScannableObjectInfo) as info:
+                info.string, _ = self.create_strg(f"{name}.STRG", strings)
+                info.static_model = model
+
+            self.pooled_scans[(strings, model)] = asset_id
+
+        return self.pooled_scans[(strings, model)]
