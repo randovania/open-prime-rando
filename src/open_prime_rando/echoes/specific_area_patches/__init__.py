ba954c25025288c26a7201077024fc5a169664d2
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-05-04 17:58:02 +0300

    Import ruff rules from rdv

--- src/open_prime_rando/echoes/specific_area_patches/__init__.py
+++ src/open_prime_rando/echoes/specific_area_patches/__init__.py
@@ -1,3 +1,3 @@
 from open_prime_rando.echoes.specific_area_patches import rebalance_patches, required_fixes, version_differences
 
-__all__ = ["required_fixes", "version_differences", "rebalance_patches"]
+__all__ = ["rebalance_patches", "required_fixes", "version_differences"]
