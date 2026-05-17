d457ddd374f6664c7636828c0c30f528825f0240
duncathan <dunc@duncathan.com>
2026-05-17 16:03:53 -0600

    fix again

--- tests/echoes/test_logbook.py
+++ tests/echoes/test_logbook.py
@@ -6,7 +6,7 @@ from unittest.mock import ANY
 from retro_data_structures.enums.echoes import InventorySlotEnum
 from retro_data_structures.formats.tree import Tree
 from retro_data_structures.properties.echoes.archetypes import EditorProperties, ScannableParameters
-from retro_data_structures.properties.echoes.objects import ScanTreeCategory, ScanTreeInventory
+from retro_data_structures.properties.echoes.objects import ScanTreeCategory, ScanTreeInventory, ScanTreeScan
 
 from open_prime_rando.dol_patching.echoes import dol_versions
 from open_prime_rando.echoes import logbook
@@ -19,7 +19,7 @@ def test_patch_logbook(prime2_editor: PatcherEditor) -> None:
     logbook.patch_logbook(prime2_editor, dol_versions.ALL_VERSIONS[0])
 
     tree = prime2_editor.get_file(0x95B61279, Tree)
-    nodes = list(tree.nodes)[-5:]
+    nodes = list(tree.nodes)[-9:]
     props = [node.get_properties() for node in nodes]
 
     assert props == [
@@ -55,5 +55,28 @@ def test_patch_logbook(prime2_editor: PatcherEditor) -> None:
             name_string_name="CobaltTranslator",
             inventory_slot=InventorySlotEnum.BeamCombo,
             scannable_parameters=ScannableParameters(scannable_info0=ANY),
+        ),
+        ScanTreeCategory(
+            editor_properties=EditorProperties(name="DarkTempleKeyHints"),
+            name_string_table=ANY,
+            name_string_name="DarkTempleKeyHints",
+        ),
+        ScanTreeScan(
+            editor_properties=EditorProperties(name="DarkAgonKeyHints"),
+            name_string_table=ANY,
+            name_string_name="DarkAgonKeyHints",
+            scannable_parameters=ScannableParameters(scannable_info0=0xA9B11356),
+        ),
+        ScanTreeScan(
+            editor_properties=EditorProperties(name="DarkTorvusKeyHints"),
+            name_string_table=ANY,
+            name_string_name="DarkTorvusKeyHints",
+            scannable_parameters=ScannableParameters(scannable_info0=0x8C669B58),
+        ),
+        ScanTreeScan(
+            editor_properties=EditorProperties(name="IngHiveKeyHints"),
+            name_string_table=ANY,
+            name_string_name="IngHiveKeyHints",
+            scannable_parameters=ScannableParameters(scannable_info0=0x813068D5),
         ),
     ]
