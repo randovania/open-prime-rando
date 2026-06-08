c5dd94a7736eb8ea38dec27611d8e16939894652
duncathan <dunc@duncathan.com>
2026-06-08 08:55:34 -0600

    check for the ETM item instead of the inventory item in the logbook
    probably fixes #155?

--- src/open_prime_rando/echoes/logbook/__init__.py
+++ src/open_prime_rando/echoes/logbook/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import itertools
 import typing
 
-from retro_data_structures.enums.echoes import InventorySlotEnum
+from retro_data_structures.enums.echoes import InventorySlotEnum, PlayerItemEnum
 from retro_data_structures.formats.hier import Hier
 from retro_data_structures.formats.scan import Scan
 from retro_data_structures.formats.tree import Tree
@@ -38,6 +38,34 @@ def _patch_dark_temple_key_scans(editor: PatcherEditor) -> None:
             info.animated_model.initial_anim = 1
 
 
+def _patch_inventory_slots(editor: PatcherEditor, tree: Tree) -> None:
+    """
+    Edits existing inventory slots, to make certain
+    logbook entries behave better in the randomizer.
+    """
+
+    # edit logbook entries to check for a different slot
+
+    inventory_entries_to_change = {
+        # Change the entry for charge combos to check for Supers instead
+        141: InventorySlotEnum.SuperMissile,
+    }
+
+    for entry_id, slot in inventory_entries_to_change.items():
+        with tree.get_node_by_id(entry_id).edit_properties(ScanTreeInventory) as tree_inventory:
+            tree_inventory.inventory_slot = slot
+
+    # edit slot assignments to check for a different inventory item
+
+    slot_assignments_to_change = {
+        # instead of dedicated logbook entry for ETM, use the normal item
+        InventorySlotEnum.EnergyTransferModule: PlayerItemEnum.EnergyTransferModulePickup,
+    }
+
+    for slot, item in slot_assignments_to_change.items():
+        editor.assign_item_to_inventory_slot(item, slot)
+
+
 def patch_logbook(editor: PatcherEditor, version: EchoesDolVersion, configuration: RandoConfiguration) -> None:
     """
     Makes the needed modifications to the Logbook:
@@ -50,9 +78,7 @@ def patch_logbook(editor: PatcherEditor, version: EchoesDolVersion, configuratio
     hierarchy = editor.get_file(0xDD79DC2A, Hier)
     hierarchy_entries = list(hierarchy.entries)
 
-    # Change the entry for charge combos to check for Supers instead
-    with tree.get_node_by_id(141).edit_properties(ScanTreeInventory) as tree_inventory:
-        tree_inventory.inventory_slot = InventorySlotEnum.SuperMissile
+    _patch_inventory_slots(editor, tree)
 
     _patch_dark_temple_key_scans(editor)
 
